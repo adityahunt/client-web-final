5047da83ac666265d4fceca1a49c7c9d8dcf4339
adityahunt <adityaranjan526@gmail.com>
2025-04-10 10:55:47 +0530

    Initial commit: Add loginandsignup assignment

---
+++ loginandsignupwithCredintials/src/components/Layout.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+function Layout({ children }: LayoutProps): React.ReactElement {
+  return (
+    // Bootstrap container for responsive width and centering
+    <div className="container mt-4">
+      {/* You could add a Navbar here later */}
+      <div className="row justify-content-center">
+        {/* Center content in a medium-sized column */}
+        <div className="col-md-16 col-lg-16">
+          {children} {/* Render the actual page content here */}
+        </div>
+      </div>
+      {/* You could add a Footer here later */}
+    </div>
+  );
+}
+
+export default Layout;
